7834b240ece9f8065214ad7a151f0f9eecddbdba
first last <jokerindex48@gmail.com>
2026-09-04 02:45:27 +0300

    feat(planner): enhance planner screen with new layout and functionality

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,17 @@
 import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
+
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+
+import * as Sentry from "@sentry/react-native";
+import { KeyboardProvider } from "react-native-keyboard-controller";
+
 import "../../global.css";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
@@ -9,10 +20,23 @@ if (!publishableKey) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  integrations: [Sentry.feedbackIntegration()],
+});
+
+export default Sentry.wrap(function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <KeyboardProvider>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack screenOptions={{ headerShown: false }} />
+        </ThemeProvider>
+      </KeyboardProvider>
     </ClerkProvider>
   );
-}
+});
