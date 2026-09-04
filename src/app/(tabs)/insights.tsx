52026062e55f25528f777cb0e6bdbfac6b604227
first last <jokerindex48@gmail.com>
2026-09-04 03:14:40 +0300

    feat(insights): add insights sections and user profile components

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,13 +1,33 @@
-import { useGroceryStore } from "@/store/grocery-store";
-import { Text, View } from "react-native";
+import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
+import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
+import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
+import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
+import SentryFeedbackButton from "@/components/insights/SentryFeedbackButton";
+import UserProfile from "@/components/insights/UserProfile";
+import TabScreenBackground from "@/components/TabScreenBackground";
+import { ScrollView } from "react-native";
 
-const insightsScreen = () => {
-  const { isLoading, items } = useGroceryStore();
+const InsightsScreen = () => {
   return (
-    <View>
-      <Text>insights</Text>
-    </View>
+    <>
+      <ScrollView
+        className="flex-1 bg-background py-4"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20, gap: 14 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TabScreenBackground />
+
+        <UserProfile />
+        <InsightsStatsSection />
+        <InsightsCategorySection />
+        <InsightsPrioritySection />
+        <ClearCompletedButton />
+      </ScrollView>
+
+      <SentryFeedbackButton />
+    </>
   );
 };
 
-export default insightsScreen;
+export default InsightsScreen;
